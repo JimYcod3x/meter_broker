b022d1f4586c2eece8406741df29a90d16d08be6
JimYcod3x <jimycodex@gmail.com>
2024-04-12 11:15:14 +0800

    test payload

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"encoding/hex"
 	"fmt"
 	"log"
@@ -25,6 +27,45 @@ var server = mqtt.New(&mqtt.Options{
 })
 
 
+func decrypt(hexPayload, hexKey, hexIV string) ([]byte, error) {
+	payload, err := hex.DecodeString(hexPayload)
+	if err != nil {
+			return nil, fmt.Errorf("error decoding payload: %v", err)
+	}
+
+	key, err := hex.DecodeString(hexKey)
+	if err != nil {
+			return nil, fmt.Errorf("error decoding key: %v", err)
+	}
+
+	iv, err := hex.DecodeString(hexIV)
+	if err != nil {
+			return nil, fmt.Errorf("error decoding IV: %v", err)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+			return nil, err
+	}
+
+	if len(payload) < aes.BlockSize {
+			return nil, fmt.Errorf("ciphertext too short")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	mode.CryptBlocks(payload, payload)
+
+	// Remove PKCS#7 padding
+	pad := int(payload[len(payload)-1])
+	if pad < 1 || pad > aes.BlockSize {
+			return nil, fmt.Errorf("invalid padding")
+	}
+	payload = payload[:len(payload)-pad]
+
+	return payload, nil
+}
+
 func main() {
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
@@ -48,10 +89,15 @@ func main() {
 
 	err = server.Subscribe(testSub, 1, func(cl *mqtt.Client, sub packets.Subscription, pk packets.Packet) {
 		hexPayload := hex.EncodeToString([]byte(pk.Payload))
+		decrptedPaylod, err := decrypt(hexPayload, "69aF7&3KY0_kk89@", "420#abA%,ZfE79@M")
+		if err != nil {
+			fmt.Println("error decrypting payload: ", err)
+		}
 		server.Log.Info("inline client received message from subscription", "client", cl.ID, "subscriptionId", sub.Identifier, "topic", pk.TopicName, "payload", string(pk.Payload))
 		fmt.Println("payload type is", reflect.TypeOf(pk.Payload))
 		fmt.Println("payload is", hexPayload)
 		fmt.Println("length of payload ", len(hexPayload))
+		fmt.Println("decrpt payload is", decrptedPaylod)
 	})
 
 	if err != nil {
